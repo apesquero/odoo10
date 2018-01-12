2a11b14c35eca19d0ef94f0e22e460faafb0f57f
apesquero <apesquero@gmail.com>
2018-01-12 11:45:21 +0100

    Update to 10

---
+++ payment_signal/models/inherit_sale_make_invoice_advance.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from openerp import models, fields, api, _
+from openerp.addons import decimal_precision as dp
+
+class SaleAdvancePaymentInv(models.TransientModel):
+    _name = "sale.advance.payment.inv"
+    _inherit = "sale.advance.payment.inv"
+
+    @api.model
+    def _get_advance_payment_method(self):
+        state_obj = self.env['sale.order'].browse(self._context.get('active_ids')).state
+        if state_obj in ('draft', 'sent'):
+            return 'fixed'
+        elif state_obj in ('sale', 'done'):
+            return 'all'
+        #TODO: Del Módulo original, no entiendo muy bien lo que hace, queda en desuso pero no lo borro
+        elif self._count() == 1:
+            sale_obj = self.env['sale.order']
+            order = sale_obj.browse(self._context.get('active_ids'))[0]
+            if all([line.product_id.invoice_policy == 'order' for line in order.order_line]) or order.invoice_count:
+                return 'all'
+        return 'delivered'
+
+    advance_payment_method = fields.Selection([
+        ('fixed', 'Down payment (fixed amount)'),
+        ('percentage', 'Down payment (percentage)'),
+        ('all', 'Invoiceable lines (deduct down payments)'),
+        ('delivered', 'Invoiceable lines')],
+        string='What do you want to invoice?',
+        default=_get_advance_payment_method,
+        required=True)
+
+    @api.onchange('advance_payment_method')
+    def _compute_amount(self):
+        sale_obj = self.env['sale.order'].browse(self._context.get('active_ids'))
+        if self.advance_payment_method == 'percentage':
+            self.amount = sale_obj.user_id.company_id.default_signal
+        if self.advance_payment_method == 'fixed':
+            self.amount = sale_obj.payment_signal
