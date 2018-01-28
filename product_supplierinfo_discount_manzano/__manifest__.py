e33c1995c638166465a2020593c50c4b5bcdc12a
Dario Lodeiros <dario@aloxa.eu>
2018-01-28 08:28:38 +0100

    Rename product_supplier_info

--- product_supplierinfo_discount_manzano/__manifest__.py
+++ product_supplierinfo_discount_manzano/__manifest__.py
@@ -7,7 +7,7 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
-    "name": "Discounts in product supplier info",
+    "name": "Discounts in product supplier info manzano",
     "version": "10.0.1.0.0",
     "author": "Tecnativa, "
               "GRAP, "
