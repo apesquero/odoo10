ac3d46135959d986ae942537a966d93df711caf3
apesquero <apesquero@gmail.com>
2018-02-21 18:07:54 +0100

    Upload Product Attribute Value Image

---
+++ product_attribute_value_image/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- encoding: utf-8 -*-
+{
+    'name': 'Product Attribute Value Image',
+    'version': "10.0.1.0",
+    'author': 'Amaro Pesquero Rodríguez',
+    'category': 'Sales Management',
+    'depends': ['product',
+                'web_tree_image',
+                'product_variant_configurator',
+                'sale_variant_configurator'
+                ],
+    'data': ['views/product_attribute_value_view.xml',
+             'views/product_configurator_attribute.xml',
+             ],
+    'installable': True,
+}
---
+++ product_attribute_value_image/models/product_configurator.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, fields, models
+
+class ProductConfiguratorAttribute(models.Model):
+    _inherit = 'product.configurator.attribute'
+
+
+    image = fields.Binary(compute='_compute_image',
+                          readonly=True, string='Image')
+
+    @api.depends('value_id')
+    def _compute_image(self):
+        for record in self:
+            record.image = record.value_id.image
